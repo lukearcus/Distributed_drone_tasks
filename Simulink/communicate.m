ecc7b103318a9a447d04dfa10472e9cd45119e77
lukearcus <45366909+lukearcus@users.noreply.github.com>
2021-08-06 14:45:24 +0100

    Add files via upload

---
+++ Simulink/communicate.m
@@ -0,0 +1,35 @@
+function [A_ineq,l_ineq] = communicate(x,N,N_j,H,Hw,delta,nu)
+
+% placeholders
+A_ineq = zeros(N*size(N_j,2),(N+1)*nu*(size(N_j,2)+1));
+l_ineq = zeros(N*size(N_j,2),1);
+
+x_bar = x(:,1);
+
+for j = 1:size(N_j,2)
+    x_bar = [x_bar;x(:,j+1)];
+end
+
+delta_x_bar = H * x_bar;
+
+for k = 2:N+1
+    
+    eta_M_k = zeros(size(N_j,2),nu*size(N_j,2)); % placeholder
+    delta_x_k = delta_x_bar((k-1)*nu*size(N_j,2)+1:k*nu*size(N_j,2)); % get k-th delta_x
+    
+    for m = 1:size(N_j,2)
+        
+        x_bar_norm = norm(delta_x_k((m-1)*nu+1:m*nu)); % get the 2 norm of x_bar
+        eta_ij_k = delta_x_k((m-1)*nu+1:m*nu)' * 1/x_bar_norm; % formulate eta ij k
+        eta_M_k(m,(m-1)*nu+1:(m-1)*nu+nu) = eta_ij_k; % populate matrix eta_M
+        
+        l_ineq((k-2)*size(N_j,2)+m) = delta + eta_ij_k * delta_x_k((m-1)*nu+1:m*nu) - x_bar_norm; % fill in l_ij
+        
+    end
+    
+    A_ineq( (k-2)*size(N_j,2)+1 : (k-1)*size(N_j,2) , :) = eta_M_k * Hw((k-1)*size(N_j,2)*nu+1:(k)*size(N_j,2)*nu,:);
+    
+end
+
+
+end
---
+++ Simulink/init_traj.m
@@ -0,0 +1,43 @@
+function x_init = init_traj(rho,Objective,constraints,model)
+
+Q = Objective.Q;
+R = Objective.R;
+N = Objective.N;
+
+Nd = 3;
+nx = size(Q,1);
+nu = size(R,1);
+
+A = model.A;
+B = model.B;
+umax = model.umax;
+umin = model.umin;
+
+N_j = constraints.N_j;
+r = constraints.r;
+x0 = constraints.x0;
+
+min_input = repmat(ones(nu,1)*umin,N,1);
+max_input = repmat(ones(nu,1)*umax,N,1);
+% - linear dynamics
+
+u_bound = [inf((N+1)*nx,1);max_input];
+l_bound = [-inf((N+1)*nx,1);min_input];
+
+
+posM = blkdiag(eye(Nd),zeros(nx-Nd)); % Matrix to take only position from the state vector x (zeros rest)
+
+Ax = kron(speye(N+1), -speye(nx)) + kron(sparse(diag(ones(N, 1), -1)), A);
+Bu = kron([sparse(1, N); speye(N)], B);
+Aeq = [Ax, Bu];
+b_eq = [-x0'; zeros(N*nx, 1)];
+P_new = blkdiag( kron(speye(N), Q+ (size(N_j,2)+1)*rho/2*posM ), Q+(size(N_j,2)+1)*rho/2*posM, kron(speye(N), R) );
+
+q = [repmat(-Q*r', N, 1); -Q*r'; zeros(N*nu, 1)];
+
+qp_opts = optimoptions('quadprog','Display','off');
+res = quadprog(P_new,q,[],[], Aeq,b_eq,l_bound,u_bound,[],qp_opts);
+x_init = res(1:nx*(N+1));
+
+
+end
